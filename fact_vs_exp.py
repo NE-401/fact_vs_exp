ecf191991fb091e29b32a5b16dc2fcffcc72e715
NE-401 <phoenix00sm@gmail.com>
2019-07-31 12:33:47 +0900

    feat: コマンドライン引数に対応

--- fact_vs_exp.py
+++ fact_vs_exp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 
 def fact(n):
     a=1
@@ -13,6 +14,18 @@ def exp(b,n):
 base=5
 limit=20
 
+if len(sys.argv) == 2:
+    limit=int(sys.argv[1])
+elif len(sys.argv) == 3:
+    limit=int(sys.argv[1])
+    base=int(sys.argv[2])
+elif len(sys.argv) > 3:
+    print('too much argument')
+    print('usage: ./fact_vs_exp.py [limit [base]]')
+
+print('exp base = {0}'.format(base))
+print('iteration limit = {0}'.format(limit))
+
 for i in range(1,limit+1):
     print('factorial of {0} = {1}'.format(i,fact(i)))
-    print('{0}^{1}            = {2}'.format(i,base,exp(base,i)))
+    print('{0}^{1}            = {2}'.format(base,i,exp(base,i)))
